bfe1fe2b48e829cef6c51d1933ffc24aa2f813f4
dale.sakamoto <daletsakamoto@gmail.com>
2021-04-21 14:13:06 -0700

    cleaning up with state

--- src/components/Comets.js
+++ src/components/Comets.js
@@ -3,7 +3,7 @@ import { useQuery } from "@apollo/react-hooks"
 import { GET_COMETS_QUERY } from "../queries/getComets.js"
 
 export default function Comets() {
-  const [state, setState] = useState([])
+  const [comets, setComets] = useState([])
   const {
     data,
     loading,
@@ -12,22 +12,12 @@ export default function Comets() {
 
   useEffect(() => {
     if (loading === false && data) {
-      setState([...state, ...data.comets])
+      setComets([...comets, ...data.comets])
     }
   }, [loading, data])
   
   if (loading) return <p>Almost there...</p>
-  // if (!data) return null
-
   if (error) return <p>{error.message}</p>
-
-  if (data !== undefined) {
-    console.log(data.comets)
-    const comets = data.comets
-    // localStorage.setItem("comets", comets)
-    // let coms = localStorage.getItem("comets")
-    // console.log(coms)
-    console.log(state)
 
   return(
     <>
@@ -42,5 +32,4 @@ export default function Comets() {
       ))}
     </>
   )
-}
 }

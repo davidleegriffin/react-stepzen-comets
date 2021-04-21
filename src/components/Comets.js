a43215ea5fd67a32b6b7d270e766765cfc8c2b10
dale.sakamoto <daletsakamoto@gmail.com>
2021-04-21 14:10:31 -0700

    data saved

--- src/components/Comets.js
+++ src/components/Comets.js
@@ -1,33 +1,42 @@
+import {useState, useEffect} from 'react'
 import { useQuery } from "@apollo/react-hooks"
 import { GET_COMETS_QUERY } from "../queries/getComets.js"
 
 export default function Comets() {
+  const [state, setState] = useState([])
   const {
     data,
     loading,
     error
   } = useQuery(GET_COMETS_QUERY)
 
-  console.log(data);
+  useEffect(() => {
+    if (loading === false && data) {
+      setState([...state, ...data.comets])
+    }
+  }, [loading, data])
   
   if (loading) return <p>Almost there...</p>
-  if (!data) return null
-  if (data) {const comets = data.comets}
+  // if (!data) return null
 
   if (error) return <p>{error.message}</p>
-  
+
   if (data !== undefined) {
+    console.log(data.comets)
     const comets = data.comets
+    // localStorage.setItem("comets", comets)
+    // let coms = localStorage.getItem("comets")
+    // console.log(coms)
+    console.log(state)
 
-  return (
-
+  return(
     <>
       <h2>Comets</h2>
       
       {comets.map(comet => (
         <ul key={comet.date}>
           <li>
-            {comet.lat}
+            Lat: {comet.lat}, Lon: {comet.lon}
           </li>
         </ul>
       ))}
